f56430f3c6ff5cbadfec6bb08fee2e40a6e3dd05
eabayev20 <eabayev20@gmail.com>
2020-11-22 14:07:11 -0500

    File renamed

---
+++ RealNumber.java
@@ -0,0 +1,31 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+getValue();
+  }
+  public boolean equals (RealNumber other) {
+    if (other == 0) {
+      return getvalue() == 0;
+    }
+    else if (getvalue() == 0) {
+      return other == 0;
+    }
+    else {
+      double error = (value-other.getValue())/value;
+    return Math.abs(error)<0.001;
+
+      }
+    }
+    public RealNumber add(RealNumber other){
+
+         return other + getValue();
+      }
