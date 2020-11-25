f693fe0e3c94063c3f90750ef0f20369bb4a1ceb
eabayev20 <eabayev20@gmail.com>
2020-11-24 22:29:15 -0500

    editied for hw 21

---
+++ Number.java
@@ -0,0 +1,39 @@
+public abstract class Number{
+  public abstract double getValue();
+
+  /*return 0 when this Number equals the other Number
+  return a negative value when this Number is smaller than the other Number
+  return a positive value when this Number is larger than the other Number
+  */
+  public int compareTo(Number other){
+    if (getValue() == other.getValue()) {
+      return 0;
+    }
+    else if (getValue() > other.getValue()) {
+      return 1;
+    }
+
+      return -1;
+
+  }
+
+  /*
+  *Return true when the % difference of the values
+  *are within 0.00001 of eachother.
+  *Special case: if one is exactly zero, the other must be exactly zero.
+  */
+  public boolean equals(Number other){
+    public boolean equals (RealNumber other) {
+      if (other.getValue() == 0) {
+        return value == 0;
+      }
+      else if (value == 0) {
+        return other.getValue() == 0;
+      }
+      else {
+        double error = (value-other.getValue())/value;
+      return Math.abs(error)<0.00001;
+
+        }
+      }
+}
